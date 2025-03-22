01da2dcf907225b0b9caf3d9ac0f7bc11802f5bf
THEPLANTATION\saatv <saatvik.barla@gmail.com>
2025-03-22 16:48:28 -0500

    I hope I dint break

--- SureCareAPI.cs
+++ SureCareAPI.cs
@@ -20,9 +20,7 @@ namespace SureCare.Functions
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
-            //more tests
-            //test again
-            
+            // final test
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
