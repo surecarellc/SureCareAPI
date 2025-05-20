d61d28abc9d541fdc62f553f87252ca75215a3d1
THEPLANTATION\saatv <saatvik.barla@gmail.com>
2025-05-20 13:42:24 -0500

    think its fixed

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
+    .ConfigureFunctionsWebApplication(builder =>
+    {
+        var app = builder.Services.BuildServiceProvider().GetRequiredService<IApplicationBuilder>();
+
+        app.UseRouting();
+
+        app.UseCors(policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:3000") // Replace with your frontend origin
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+
+        app.UseEndpoints(endpoints => { });
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddCors(); // 🔥 Must register it here
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
     })
